276ae358a3baeb2ef4ecdfd553ad162310488f68
Madhusuthanan-B <madhub081011@gmail.com>
2023-07-08 22:28:52 +0530

    Log setup in controller

--- task-manager-app/src/tasks/tasks.controller.ts
+++ task-manager-app/src/tasks/tasks.controller.ts
@@ -1,6 +1,5 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, Logger } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
@@ -8,15 +7,18 @@ import { Task } from './task.entity';
 
 @Controller('tasks')
 export class TasksController {
+    private logger = new Logger('TasksController');
     constructor(private tasksService: TasksService) { }
 
     @Get()
     getTasks(@Query() filterDto: GetTasksFilterDto): Promise<Task[]> {
+        this.logger.log(`Trying to retrive tasks`);
         return this.tasksService.getTasksWithFilters(filterDto);
     }
 
     @Get('/:id')
     async getTaskById(@Param('id') id: string): Promise<Task> {
+        this.logger.log(`Trying to retrive the task ${id}`);
         return await this.tasksService.getTaskById(id);
     }
 
@@ -27,6 +29,7 @@ export class TasksController {
 
     @Delete('/:id')
     deleteTaskById(@Param('id') id: string) {
+        this.logger.log(`Trying to delete the task ${id}`);
         return this.tasksService.deleteTaskById(id);
     }
 
@@ -34,6 +37,7 @@ export class TasksController {
     updateTaskStatus(
         @Param('id') id: string,
         @Body() updateTaskStatusDto: UpdateTaskStatusDto): Promise<Task> {
+        this.logger.log(`Trying to update the task ${id} with ${JSON.stringify(updateTaskStatusDto)}`);
         const { status } = updateTaskStatusDto;
         return this.tasksService.updateTaskStatus(id, status);
     }
